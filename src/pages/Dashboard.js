cc36fd41eac6cd140ab4e94f4edd419c0ba4560b
Benoit Rajalu <rajalubenoit@gmail.com>
2017-08-30 15:57:31 +0200

    Components that will be used as traditional "pages" (templates / views) will live in a /pages subfolder

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="Dashboard">
+        <p>Hey hey I am the dashboard</p>
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
