8fe59a31311f7237ac0c5eac674896b0c519771c
Benoit Rajalu <rajalubenoit@gmail.com>
2017-08-30 15:56:37 +0200

    Index is now fully empty, top-level container for the whole App and shall never be touched ever again. App.js is the routing and base structural container of the App (as its name suggests) and now features the base react-router logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard.js';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <BrowserRouter>
+        <div className="App">
+          <div className="App-header">
+            <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
+            <h2>This is a work in progress</h2>
+          </div>
+          <main>
+            <Route exact path="/" component={Dashboard}/>
+          </main>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </BrowserRouter>
     );
   }
 }
