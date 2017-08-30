de375c2f15a4f62cab7237f8879f3c325981ce5a
Benoit Rajalu <rajalubenoit@gmail.com>
2017-08-30 15:58:16 +0200

    All tests are written per components in a /test subfolder for clarity

---
+++ src/tests/Dashboard.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <MemoryRouter>
+      <Dashboard />
+    </MemoryRouter>, 
+  div);
+});
