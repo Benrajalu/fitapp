c0a5ddaa8187f1382ef425ccc582b3dc0b3dcbb9
Benoit Rajalu <rajalubenoit@gmail.com>
2017-09-04 09:52:02 +0200

    Users on the dashboard now have access to quick options for basic actions

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Dashboard extends Component {
   render() {
@@ -7,6 +8,18 @@ class Dashboard extends Component {
         <div className="container">
           <div className="page-header">
             <h1>Dashboard</h1>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="col-md-4">
+            <button className="btn btn-primary btn-lg btn-block">Lancer un entraînement</button>
+          </div>
+          <div className="col-md-4">
+            <Link to='/new-routine' className="btn btn-default btn-lg btn-block">Créer un entraînement</Link>
+          </div>
+          <div className="col-md-4">
+            <Link to='/settings' className="btn btn-default btn-lg btn-block">Paramètres</Link>
           </div>
         </div>
       </div>
