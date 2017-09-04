a5367f815b24a4030612b9656d974c36a264f461
Benoit Rajalu <rajalubenoit@gmail.com>
2017-09-04 17:51:37 +0200

    The workout history detail is a wrapper block that creates as many set entries as the provided workout log requests using a child "exercise" component

---
+++ src/blocks/WorkoutHistoryDetail.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import ExerciseListing from '../blocks/ExerciseListing';
+
+class WorkoutHistoryDetail extends Component {
+  render() {
+    const workoutExercices = this.props.contents.exercises;
+    const exercisesDatabase = this.props.exercisesDatabase;
+    const listExercises = workoutExercices.map((value) => {
+      return <ExerciseListing key={value.exerciceId.toString()} exerciseData={value} exercisesDatabase={exercisesDatabase} status="past" />
+    });
+
+    return (
+      <div className="panel panel-default routine-card">
+        <div className="panel-heading">
+          <h3 className="panel-title">{this.props.contents.routineName}</h3>
+        </div>
+        <div className="panel-body">
+          {listExercises}
+        </div>
+      </div>
+    )
+  }
+}
+
+WorkoutHistoryDetail.propTypes = {
+  contents: PropTypes.object.isRequired,
+  exercisesDatabase: PropTypes.array.isRequired
+}
+
+export default WorkoutHistoryDetail;
