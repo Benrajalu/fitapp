0ed33d244e4f24dd53b87869704b458ec1fc55ae
Benoit Rajalu <rajalubenoit@gmail.com>
2017-10-04 15:06:06 +0200

    Also testing the "account" part of the page

--- src/tests/Settings.test.js
+++ src/tests/Settings.test.js
@@ -42,9 +42,25 @@ describe('when managing settings', () => {
   });
 
   test("it updates settings flawlessly", () => {
+    // Checking the first disc weight, making it available
     settings.find('input[type="checkbox"]').first().simulate('change');
     expect(settings.find('input[type="checkbox"]').first().props().checked).toBeTruthy();
+    // Unchecking it
     settings.find('input[type="checkbox"]').first().simulate('change');
     expect(settings.find('input[type="checkbox"]').first().props().checked).not.toBeTruthy();
+  });
+
+  test("it won't move if you try to feed it a bad email", () => {
+    settings.setState({
+      userEmail: "bad email"
+    });
+    settings.find('input[name="userEmail"]').simulate('change');
+    expect(settings.find('button[type="submit"]').props().disabled).toBeTruthy();
+    // Unchecking it
+    settings.setState({
+      userEmail: "test@test.com"
+    });
+    settings.find('input[name="userEmail"]').simulate('change');
+    expect(settings.find('button[type="submit"]').props().disabled).not.toBeTruthy();
   });
 });
