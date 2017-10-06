8c1c0505d30a532112cf5f429b85460afeb0c63d
Benoit Rajalu <rajalubenoit@gmail.com>
2017-10-06 12:23:33 +0200

    Laying down some tools so new users can be distinguished from returning ones

--- src/pages/Callback.js
+++ src/pages/Callback.js
@@ -1,15 +1,44 @@
-import { Component } from 'react';
-import { setIdToken, setAccessToken } from '../utils/AuthService';
+import React, { Component } from 'react';
+import { setIdToken, setAccessToken, isLoggedIn, getUserInfo, userData } from '../utils/AuthService';
+import database from '../data/users.json';
+import {Redirect} from 'react-router';
 
 class Callback extends Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile:false
+    };
+  }
+
+  componentWillMount() {
     setAccessToken();
     setIdToken();
+    const isLogged = isLoggedIn();
+    
+    if(isLogged){
+      if (!userData) {
+        getUserInfo((err, profile) => {
+          this.setState({profile})
+        });
+      } else {
+        this.setState({
+          profile: userData
+        })
+      }
+    }
+
     window.location.href = "/";
   }
 
   render() {
-    return null;
+    var trueUser = database.filter((obj) => obj.id === this.state.profile.sub)[0];
+    console.log(trueUser);
+    return(
+      <div>
+        { this.state.profile ? <Redirect push to={{ pathname:'/'}} /> : false }
+      </div>
+    );
   }
 }
 
