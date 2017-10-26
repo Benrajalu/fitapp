e4f558a030c53dcf657ada37b3c8c37a9989bcf1
Benoit Rajalu <rajalubenoit@gmail.com>
2017-10-26 19:22:35 +0200

    Users can now list their created routines

--- src/pages/AllRoutines.js
+++ src/pages/AllRoutines.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {firebaseAuth, database} from '../utils/fire';
 
 import Routines from '../blocks/Routines';
 
@@ -12,28 +13,58 @@ class AllRoutines extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading:true,
       routinesList:[], 
       exercises: [], 
       user:{}
     }
   }
   componentDidMount() {
-    this.setState({
-      routinesList: userData[0].routines, 
-      exercises: exercisesDatabase, 
-      user:userData[0]
+    const _this = this;
+    firebaseAuth.onAuthStateChanged(function(user) {
+      if (user) {
+        database.collection('users').doc(user.uid).collection('routines').get().then((snapshot) => {
+          const output = [];
+          snapshot.forEach((doc) => {
+            let data = doc.data();
+                data.id = doc.id;
+            output.push(data);
+          });
+          _this.setState({
+            routinesList: output.length > 0 ? output : false, 
+            user: {id: user.uid}
+          });
+          console.log(_this.state.routinesList);
+        });
+
+        database.collection('exercises').get().then((snapshot) => {
+          const output = [];
+          snapshot.forEach((doc) => {
+            output.push(doc.data());
+          });
+          _this.setState({
+            exercises: output,
+            loading: false
+          })
+        });
+      } else {
+        firebaseAuth.signOut();
+      }
     });
   }
   
   render() {
     // Routines are sorted by most recently performed to never used ever
-    const routines = this.state.routinesList.sort((a, b) => {
-      var aDate = a.lastPerformed.split('/');
-      var bDate = b.lastPerformed.split('/');
-      var c = new Date('20' + aDate[2], aDate[1] - 1, aDate[0]);
-      var d = new Date('20' + bDate[2], bDate[1] - 1, bDate[0]);
-      return c>d ? -1 : c<d ? 1 : 0;
-    });
+    let routines =[];
+    if(this.state.routinesList.length > 0 && this.state.routinesList){
+      routines = this.state.routinesList.sort((a, b) => {
+        var aDate = a.lastPerformed.split('/');
+        var bDate = b.lastPerformed.split('/');
+        var c = new Date('20' + aDate[2], aDate[1] - 1, aDate[0]);
+        var d = new Date('20' + bDate[2], bDate[1] - 1, bDate[0]);
+        return c>d ? -1 : c<d ? 1 : 0;
+      });
+    }
 
     return (
       <div className="AllRoutines">
@@ -42,7 +73,17 @@ class AllRoutines extends Component {
             <h1>Mes entraînements</h1>
             <Link className="btn btn-default" to='/new-routine'>Créer un nouvel entraînement</Link>
           </div>
-          <Routines list={routines} exercisesDatabase={this.state.exercises} editable="true" user={this.state.user} />
+          {this.state.loading ? 
+            <p>Chargement de vos données...</p>
+            :
+            <div>
+              {this.state.routinesList.length > 0 && this.state.routinesList ? 
+                <Routines list={routines} exercisesDatabase={this.state.exercises} editable="true" user={this.state.user} />
+                :
+                <div className="alert alert-warning">Vous n'avez pas encore créé d'entraînement !</div>
+              }
+            </div>
+          }
         </div>
       </div>
     )
